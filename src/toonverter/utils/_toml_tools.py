927f6824428759291a080b641a95915b81bffb84
Saidatta Teja Ghanta <dattateja44@gmail.com>
2025-12-12 18:49:25 +0530

    tomlib version fix for python

--- src/toonverter/utils/_toml_tools.py
+++ src/toonverter/utils/_toml_tools.py
@@ -4,16 +4,18 @@ from types import ModuleType
 
 
 # Handle TOML parser import (tomllib for Python 3.11+, tomli backport otherwise)
-tomllib: ModuleType | None
+_tomllib_module: ModuleType | None
 
 if sys.version_info >= (3, 11):
-    import tomllib
+    import tomllib as _tomllib_module
 else:
     try:
-        import tomli as tomllib
+        import tomli as _tomllib_module
     except ImportError:
-        tomllib = None
+        _tomllib_module = None
         warnings.warn(
             "The 'tomli' library is not installed. TOML configuration will not be loaded.",
             stacklevel=2,
         )
+
+tomllib = _tomllib_module
